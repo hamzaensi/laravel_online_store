bb70c06e52e9fdfd912b6d51cbbd8895995a4337
hamza <hamzaensi@gmail.com>
2023-01-24 12:37:43 +0100

    product index and show

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,14 +6,16 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         
         $viewData = [];
         $viewData["title"] = "Home Page - Online Store";
         return view('home.index')->with("viewData", $viewData);
     }
     
-    public function about(){
+    public function about()
+    {
         
         $viewData = [];
         $viewData["title"] = "About us - Online Store";
@@ -21,6 +23,5 @@ class HomeController extends Controller
         $viewData["description"] = "This is an about page ...";
         $viewData["author"] = "Developed by: Z.Hamza";
         return view('home.about')->with("viewData", $viewData);
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[HomeController::class,'index'])->name("home.index");
+Route::get('/', [HomeController::class, 'index'])->name("home.index");
 
 Route::get('about', [HomeController::class,'about'])->name("home.about");
+
+Route::get('/products', [ProductController::class, 'index'])->name("product.index");
+Route::get('/products/{id}',[ProductController::class, 'show'])->name("product.show");
